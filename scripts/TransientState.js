421a59061c0490fa88b38cbdd82f2cdbc65597f8
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-24 15:36:18 -0500

    changed to Map data structure

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -1,19 +1,18 @@
 // Set up the transient state data structure and provide initial values
 
-const transientState = {
-    "ownsBlueJeans": false,
-    "socioLocationId": 0
-}
+const transientState = new Map()
+    transientState.set("ownsBlueJeans", true)
+    transientState.set("socioLocationId", 0)
 
 
 // Functions to modify each property of transient state
 
 export const setOwnsBlueJeans = (chosenOwnership) => {
-    transientState.ownsBlueJeans = chosenOwnership
+    transientState.set("ownsBlueJeans", chosenOwnership)
 }
 
 export const setSocioLocationId = (chosenLocation) => {
-    transientState.socioLocationId = chosenLocation
+    transientState.set("socioLocationId", chosenLocation)
 }
 
 
@@ -25,7 +24,7 @@ export const saveSurveySubmission = async () => {
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(transientState)
+        body: JSON.stringify(Object.fromEntries(transientState))
     }
 
     const response = await fetch("http://localhost:8088/submissions", postOptions)
